06e8cedb5ff4aa20aef9ef1e2fa3598a1d3de145
XimenaLoya <ximena.loya@cetys.edu.mx>
2023-03-22 19:39:48 -0700

    again commit

---
+++ src/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1/basededatos', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(db => console.log('Ya esta conectado'))
+.catch((err) => console.error(err));
